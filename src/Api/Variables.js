3c95e60846d2685012ce6c627166aeb7dae18c14
ahmedmeid99 <ahmedalbakrycool@gmail.com>
2024-07-04 22:46:07 +0300

    update url

--- src/Api/Variables.js
+++ src/Api/Variables.js
@@ -1,2 +1,3 @@
 //export const URL = "https://localhost:7162";
-export const URL ="http://ahmedeid99-001-site1.gtempurl.com"
+// export const URL ="http://ahmedeid99-001-site1.gtempurl.com";
+export const URL ="http://ahmed199963-001-site1.htempurl.com"
